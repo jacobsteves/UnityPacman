41bdb147633b87bce1d8a49a14f55c311a27c44c
Jacob Steves <jgsteves@uwaterloo.ca>
2017-05-26 22:23:53 -0400

    Initial code commit

---
+++ Assets/scripts/activatePacMan.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activatePacMan : MonoBehaviour {
+
+	IEnumerator Wait(){
+		yield return new WaitForSeconds (5.0f); //waiting to fade, so it doesnt happen the second you go outside
+		float fadeTime = GameObject.Find ("Pac-man Trigger").GetComponent<fader>().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime); //fading
+		yield return new WaitForSeconds (5.0f); //this last one is for dramatic effect
+		Application.LoadLevel("levelOne");
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		StartCoroutine(Wait());
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # UnityPacman
-A Unity remake of the classic game pac-man. 
+A Unity remake of the classic game pac-man.
+
+[Demo!](http://www.jacobsteves.ca/pacman)
+
+## Setup
+Download the newest version of [Unity.](https://unity3d.com/get-unity/download).
+Clone this repo and simply open it up within unity.
+Then enjoy your pac-man!
